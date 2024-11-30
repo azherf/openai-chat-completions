0ace1b24cf58ef50877019cc50aea666c439a1fe
Azher Farooqi <azher@momentum.io>
2024-11-30 13:57:21 -0800

    style: Add tailwindcss. and beautify the elements a bit

--- src/App.js
+++ src/App.js
@@ -16,10 +16,12 @@ const App = () => {
     setResponse(data.content)
   }
   return (
-      <div>
-        <input onChange={e => setText(e.target.value)} />
-        <button onClick={ getCompletion }>Submit</button>
-        <p>{response}</p>
+      <div className="m-5 p-5">
+          <div className="flex items-center">
+            <textarea className="border rounded-sm w-full p-2" onChange={e => setText(e.target.value)} />
+            <button className="border-blue-500 bg-blue-500 text-white rounded-md hover:bg-blue-400 p-5 ml-2" onClick={ getCompletion }>Submit</button>
+          </div>
+          <p className=" whitespace-pre-line mt-5 w-full h-100 bg-gray-100 p-5">{response}</p>
       </div>
   )
 }
